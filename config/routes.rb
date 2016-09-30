eb32a4267736fdcdaffa321ff78bc415701f847b
Bang Nguyen <bangnv77@gmail.com>
2016-09-30 09:56:51 -0700

    create posts_controller and define index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show]
 
   resources :users do
     resources :posts
   end
+
 end
