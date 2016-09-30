68cc842c6dcba9137309a0213b378df45afed341
Bang Nguyen <bangnv77@gmail.com>
2016-09-30 09:44:18 -0700

    set up routes for Posts and add Sign Up navigation on the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   resources :users, only: [:index, :show]
+
+  resources :users do
+    resources :posts
+  end
 end
